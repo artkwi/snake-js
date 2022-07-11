7e06b6d30c2b675223c68fc3189eb2206d9d90a8
artkwi <kwiatkowski.artur.poczta@gmail.com>
2022-07-11 13:37:55 +0200

    New draw approach - update elemans. Styling ang js

--- draw.js
+++ draw.js
@@ -1,8 +1,39 @@
 import { getSnakePosition } from "./snake.js";
 import { getFoodPosition } from "./food.js";
-import { GAME_SIZE } from "./index.js";
+import { GAME_SIZE, isGameStarted } from "./index.js";
 
 export const draw = () => {
+  if (!isGameStarted) {
+    initialBoardDraw();
+  } else {
+    laterBoardDraw();
+  }
+
+  // wynik
+  const x = document.getElementById("x");
+  const y = document.getElementById("y");
+
+  x.textContent = getSnakePosition()[0].x;
+  y.textContent = getSnakePosition()[0].y;
+
+  // end wynik
+
+  getSnakePosition().map(({ x, y }, i) => {
+    const bodyElements = document.getElementsByClassName(
+      `board-element-${x}-${y}`
+    );
+    if (bodyElements.length) {
+      if (i === 0) {
+        bodyElements?.[0].classList?.add("body-segment--head");
+      }
+      bodyElements?.[0].classList?.add("body-segment");
+    }
+  });
+
+  drawFood();
+};
+
+const initialBoardDraw = () => {
   const gameBoard = document.getElementById("game-board");
   gameBoard.innerHTML = "";
 
@@ -16,17 +47,28 @@ export const draw = () => {
       gameBoard.appendChild(boardElement);
     }
   }
+};
 
-  getSnakePosition().map(({ x, y }) => {
-    const bodyElements = document.getElementsByClassName(
-      `board-element-${x}-${y}`
-    );
-    bodyElements[0].classList.add("body-segment");
-  });
+const laterBoardDraw = () => {
+  for (let i = 0; i < GAME_SIZE; i++) {
+    for (let j = 0; j < GAME_SIZE; j++) {
+      const boardElement = document.getElementsByClassName(
+        `board-element-${i}-${j}`
+      );
+      boardElement[0].classList.remove(
+        `body-segment`,
+        `food-segment`,
+        `body-segment--head`
+      );
+    }
+  }
+};
 
+const drawFood = () => {
   const { x, y } = getFoodPosition();
   const bodyElements = document.getElementsByClassName(
     `board-element-${x}-${y}`
   );
+
   bodyElements[0].classList.add("food-segment");
 };
--- index.js
+++ index.js
@@ -2,12 +2,16 @@ import { draw } from "./draw.js";
 import { update as updateFood } from "./food.js";
 import { update as updateSnake, getSnakePosition, onSnake } from "./snake.js";
 
-export const GAME_SIZE = 15;
+export const GAME_SIZE = 11;
 export const GAME_SPEED = 4;
+
+export let isGameStarted = false;
+export let points
 
 const main = () => {
   update();
   draw();
+  isGameStarted = true;
 };
 
 window.setInterval(main, 1000 / GAME_SPEED);
@@ -18,10 +22,19 @@ const update = () => {
   checkForDeath();
 };
 
+const restartGame = () => {
+  window.location = "/";
+};
+
 const checkForDeath = () => {
   const snakePosition = getSnakePosition();
   const isHeadIsOnBody = onSnake(snakePosition[0], { withoutHead: true });
-  if (isHeadIsOnBody) {
-    console.log("koniec");
+  const isSnakeOutsideGame =
+    snakePosition[0].x < 0 ||
+    snakePosition[0].y < 0 ||
+    snakePosition[0].x >= GAME_SIZE ||
+    snakePosition[0].y >= GAME_SIZE;
+  if (isHeadIsOnBody || isSnakeOutsideGame) {
+    // confirm("Koniec gry.", restartGame());
   }
 };
