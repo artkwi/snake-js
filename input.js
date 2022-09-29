d4bf373982723ba621d9fff646054f066f192509
artkwi <kwiatkowski.artur.poczta@gmail.com>
2022-09-29 17:13:38 +0200

    handle click on key arrow added

--- input.js
+++ input.js
@@ -2,8 +2,7 @@ import { getSnakePosition } from "./snake.js";
 
 export let inputDirection = { x: -1, y: 0 };
 
-document.addEventListener("keydown", (e) => {
-  const key = e.key;
+const setDirection = (key) => {
   switch (key) {
     case "ArrowLeft":
       if (getInputDirection().y === 0) {
@@ -28,6 +27,40 @@ document.addEventListener("keydown", (e) => {
     default:
       break;
   }
+}
+
+document.addEventListener("keydown", (e) => {
+  const key = e.key;
+  setDirection(key);
 });
 
 export const getInputDirection = () => inputDirection;
+
+addEventListener('click',(e) => {
+  const keyNumber = e.target.getAttribute("data-key");
+  let keyName = null;
+
+  if (keyNumber) {
+    switch (Number(keyNumber)) {
+      case 37:
+        keyName = "ArrowLeft";
+        break;
+      case 39:
+        keyName = "ArrowRight";
+        break;
+      case 38:
+        keyName = "ArrowUp";
+        break;
+      case 40:
+        keyName = "ArrowDown";
+        break;
+    
+      default:
+        break;
+    }
+    
+    if (keyName) {
+      setDirection(keyName);
+    }
+  }
+} )
