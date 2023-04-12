35b7b43d5a8ce3d8e1f8b0fdaf24aeb17257f667
artkwi <kwiatkowski.artur.poczta@gmail.com>
2023-04-12 13:47:08 +0200

    handle play music on keydown

--- keys.js
+++ keys.js
@@ -1,11 +1,15 @@
-window.addEventListener('keyup', arrowUp)
-window.addEventListener('keydown', arrowDown)
+window.addEventListener("keyup", arrowUp);
+window.addEventListener("keydown", arrowDown);
 
 function arrowDown(e) {
   const key = document.querySelector(`.arrow-key[data-key="${e.keyCode}"]`);
-  key.classList.add('press')
+  if (key) {
+    key.classList.add("press");
+  }
 }
 function arrowUp(e) {
   const key = document.querySelector(`.arrow-key[data-key="${e.keyCode}"]`);
-   key.classList.remove('press')
+  if (key) {
+    key.classList.remove("press");
+  }
 }
--- music.js
+++ music.js
@@ -1,12 +1,20 @@
-const playButtonMusic = document.getElementById('play-music-button');
-const playerElement = document.getElementById('player');
+const playButtonMusic = document.getElementById("play-music-button");
+const playerElement = document.getElementById("player");
 
-playButtonMusic.addEventListener('click', (e) => {
+const playMusic = () => {
   if (playerElement.paused) {
     playerElement.play();
-    playButtonMusic.textContent = 'Pause music';
+    playButtonMusic.textContent = "Pause music (M)";
   } else {
     playerElement.pause();
-    playButtonMusic.textContent = 'Play music';
+    playButtonMusic.textContent = "Play music (M)";
   }
-})
+};
+
+playButtonMusic.addEventListener("click", playMusic);
+
+document.addEventListener("keydown", (e) => {
+  if (e.key == "m") {
+    playMusic();
+  }
+});
